ae61476f747ed9217b8c41c039fe740f565ca28b
zhanghuizhen <zhanghuizhen36kr.com>
2019-01-07 17:53:12 +0800

    feat:调试路由

---
+++ app/Http/Controllers/Admin/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 后台
+ * Date: 2018/12/27
+ * Time: 16:24
+ */
+
+namespace App\Http\Controllers\Admin;
+
+
+use App\Http\Controllers\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return 'here is admin 后台';
+    }
+}
---
+++ app/Http/Controllers/Home/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 前台
+ * Date: 2018/12/27
+ * Time: 16:25
+ */
+
+namespace App\Http\Controllers\Home;
+
+
+use App\Http\Controllers\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return 'this is home 前台';
+    }
+}
